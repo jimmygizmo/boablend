18c49c41d997e9e7adb88e8b0ef187977cb257fa
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-05-16 00:20:29 -0700

    added a TODO for helpful logging from the Blender hook script

--- blender_internal_python_hook.py
+++ blender_internal_python_hook.py
@@ -5,6 +5,10 @@ import bpy
 # we do not yet have bpy installed externally (compile errors) and so any 'import bpy'
 # statements done in external python files will show an error in the IDE.
 
+# TODO: Add code here to detect CWD and log/print it to help show the Blender python
+# context, such as CWD. os.getcwd()
+# This can help lend understanding to how boablend imports are handled and other
+# issues specific to using Python to automate Blender, especially in an advanced manner.
 
 # The code in this file goes into a text document in a blender file in order to maintain
 # the main python code file external to Blender.
