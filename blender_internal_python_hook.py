a1dd0fcc639eb9be2d67814232e4ca2dca414b4f
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-05-17 12:50:51 -0700

    generic blender 2.79 file added which has hook code installed and nothing else. git lfs tracking for *.blend at repo root. simplified boablend hook versioning. comment and filename clarified.

--- blender_internal_python_hook.py
+++ blender_internal_python_hook.py
@@ -2,11 +2,10 @@
 ###############################################################################
 ################################ BOABLEND HOOK ################################
 # boablend_hook_version = '2019-001'
-# boablend_hook_hash = '18c49c41d997e9e7adb88e8b0ef187977cb257fa'
-# boablend_hook_date = 'Thu May 16 00:20:29 2019 -0700'
 ###############################################################################
-# Install all of the code in this file into a Text object within Blender.
-# Click 'Run Script' on this Text object or perform an equivalent action to
+# Install all of the code in this file into a Text Object within Blender.
+# It is recommended to name this Text Object 'boablend_hook'.
+# Click 'Run Script' on this Text Object or perform an equivalent action to
 # run the current Boablend project configuration.
 # Other modes of execution may be available in the future.
 ###############################################################################
@@ -14,13 +13,14 @@
 import bpy  # See comments below about bpy import errors showning in your IDE.
 import os
 
-# Print status and diagnostic info to STDOUT.
+# Set verbose to True to print status and diagnostic info to STDOUT.
+# This flag only affects output from this hook code.
 verbose = True
 
 ###############################################################################
 
 ###################### BOABLEND ENTRY POINT CONFIGURATION #####################
-# The executable python file which imports most of this projects dependencies
+# The executable python file which imports most of this project's dependencies
 # and runs the main code. It may never be necessary to change this and in fact
 # it may not be recommended to. To fork/customize execution in your Boablend
 # project, you will probably have a better method and location to do this
