ad21797deda27e01c0b51bd189a5c4a6929cc17a
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-05-16 21:33:52 -0700

    added script to start blender app from console so python loggin and errors can be seen. lots of improvement to hook code.

--- blender_internal_python_hook.py
+++ blender_internal_python_hook.py
@@ -1,23 +1,70 @@
-import bpy
-# Since bpy is imported here within Blenders internal Python environment, we do not
-# need to import by in the external python code, which is helpful since we will be
-# working on that code externally with an IDE (VSCode) and under the current setup
-# we do not yet have bpy installed externally (compile errors) and so any 'import bpy'
-# statements done in external python files will show an error in the IDE.
 
-# TODO: Add code here to detect CWD and log/print it to help show the Blender python
-# context, such as CWD. os.getcwd()
-# This can help lend understanding to how boablend imports are handled and other
-# issues specific to using Python to automate Blender, especially in an advanced manner.
+###############################################################################
+################################ BOABLEND HOOK ################################
+# boablend_hook_version = '2019-001'
+# boablend_hook_hash = '18c49c41d997e9e7adb88e8b0ef187977cb257fa'
+# boablend_hook_date = 'Thu May 16 00:20:29 2019 -0700'
+###############################################################################
+# Install all of the code in this file into a Text object within Blender.
+# Click 'Run Script' on this Text object or perform an equivalent action to
+# run the current Boablend project configuration.
+# Other modes of execution may be available in the future.
+###############################################################################
 
-# The code in this file goes into a text document in a blender file in order to maintain
-# the main python code file external to Blender.
+import bpy  # See comments below about bpy import errors showning in your IDE.
+import os
 
-# The following Python script file argument should be relative to the
-# current .blend file.
+# Print status and diagnostic info to STDOUT.
+verbose = True
 
-filepath = bpy.path.abspath("//test_boablend_camera.py")
+###############################################################################
+
+###################### BOABLEND ENTRY POINT CONFIGURATION #####################
+# The executable python file which imports most of this projects dependencies
+# and runs the main code. It may never be necessary to change this and in fact
+# it may not be recommended to. To fork/customize execution in your Boablend
+# project, you will probably have a better method and location to do this
+# within the entry point file itself and that would usually be more
+# appropriate.
+
+# The following path must be expressed:
+# RELATIVE TO THE CURRENT BLEND FILE CONTAINING THIS HOOK CODE.
+boablend_entry_point = 'test_boablend_camera.py'
+
+###############################################################################
+
+# NOTE: IDE Indication of bpy import errors:
+# Since bpy is imported here within Blenders internal Python environment, we
+# do not need to import bpy in the external python code, and that's not
+# currently possible with the setup this project currently is using anyhow.
+# This project is setup to use the VSCode (or Atom, IDEA CE, or a similar IDE)
+# to edit all of the source code. Under the current setup bpy is not yet
+# installed externally due to compilation errors in the first pass
+# installation attempt, and so any 'import bpy' statements done in external
+# python files will show an error in the IDE.
+# Note that a small piece of hook code will reside in the blender file, but
+# this will likely always be a very small amount of code. If any changes are
+# ever needed to the hook code or if it is updated along with this project,
+# you will need to manually (cut/paste) the updated hook code.
+
+current_working_directory = os.getcwd()
+
+if verbose:
+    print("Blender file current working directory: ")
+    print(current_working_directory)
+
+if verbose:
+    print("Boablend entry point filename: {}".\
+        format(boablend_entry_point))
+
+filepath = bpy.path.abspath("//{}".format(boablend_entry_point))
+
+if verbose:
+    print("Executing entry point at: ")
+    print(filepath)
+
 exec(compile(open(filepath).read(), filepath, 'exec'))
+
 
 ##
 #
---
+++ open_blender_in_console.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+# Use a console command like the one below to launch the Blender application in a way which leaves a console window open in which you can see Python errors. This is currently the only way I know of to see Python error details while running and debugging Python code within Blender.
+
+# The below command is for Max OS. The command and the behavior may be a little different on Windows and Linux of course.
+
+open /Applications/Blender/blender.app/Contents/MacOS/blender
+
+##
+#
+
