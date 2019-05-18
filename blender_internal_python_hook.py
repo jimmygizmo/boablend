682348012e9ad656751e581b65a40525979e6228
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-05-18 15:47:37 -0700

    remormatted hook code for 100 columns so now everything is at 100 cols. hook code version is now 2019-004 updated hook code in the 2.79 blender file.

--- blender_internal_python_hook.py
+++ blender_internal_python_hook.py
@@ -1,14 +1,13 @@
 
-###############################################################################
-################################ BOABLEND HOOK ################################
-# boablend_hook_version = '2019-003'
-###############################################################################
+####################################################################################################
+########################################## BOABLEND HOOK ###########################################
+# boablend_hook_version = '2019-004'
+####################################################################################################
 # Install all of the code in this file into a Text Object within Blender.
 # It is recommended to name this Text Object 'boablend_hook'.
-# Click 'Run Script' on this Text Object or perform an equivalent action to
-# run the current Boablend project configuration.
-# Other modes of execution may be available in the future.
-###############################################################################
+# Click 'Run Script' on this Text Object or perform an equivalent action to run the current Boablend
+# project configuration. Other modes of execution may be available in the future.
+####################################################################################################
 
 import bpy  # See comments below about bpy import errors showning in your IDE.
 import os
@@ -17,35 +16,31 @@ import os
 # This flag only affects output from this hook code.
 verbose = True
 
-###############################################################################
+####################################################################################################
 
-###################### BOABLEND ENTRY POINT CONFIGURATION #####################
-# The executable python file which imports most of this project's dependencies
-# and runs the main code. It may never be necessary to change this and in fact
-# it may not be recommended to. To fork/customize execution in your Boablend
-# project, you will probably have a better method and location to do this
-# within the entry point file itself and that would usually be more
-# appropriate.
+################################ BOABLEND ENTRY POINT CONFIGURATION ################################
+# The executable python file which imports most of this project's dependencies and runs the main
+# code. It may never be necessary to change this and in fact it may not be recommended to. To fork
+# or customize execution in your Boablend project, you will probably have a better method and
+# location to do this within the entry point file itself and that would usually be more appropriate.
 
 # The following path must be expressed:
 # RELATIVE TO THE CURRENT BLEND FILE CONTAINING THIS HOOK CODE.
 boablend_entry_point = 'boablend_start.py'
 
-###############################################################################
+####################################################################################################
 
 # NOTE: IDE Indication of bpy import errors:
-# Since bpy is imported here within Blenders internal Python environment, we
-# do not need to import bpy in the external python code, and that's not
-# currently possible with the setup this project currently is using anyhow.
-# This project is setup to use the VSCode (or Atom, IDEA CE, or a similar IDE)
-# to edit all of the source code. Under the current setup bpy is not yet
-# installed externally due to compilation errors in the first pass
-# installation attempt, and so any 'import bpy' statements done in external
-# python files will show an error in the IDE.
-# Note that a small piece of hook code will reside in the blender file, but
-# this will likely always be a very small amount of code. If any changes are
-# ever needed to the hook code or if it is updated along with this project,
-# you will need to manually (cut/paste) the updated hook code.
+# Since bpy is imported here within Blenders internal Python environment, we do not need to import
+# bpy in the external python code, and that's not currently possible with the setup this project
+# currently is using anyhow. This project is setup to use the VSCode (or Atom, IDEA CE, or a similar
+# IDE) to edit all of the source code. Under the current setup bpy is not yet installed externally
+# due to compilation errors in the first pass installation attempt, and so any 'import bpy'
+# statements done in external python files will show an error in the IDE.
+#
+# Note that a small piece of hook code will reside in the blender file, but this will likely always
+# be a very small amount of code. If any changes are ever needed to the hook code or if it is
+# updated along with this project, you will need to manually (cut/paste) the updated hook code.
 
 
 if verbose:
