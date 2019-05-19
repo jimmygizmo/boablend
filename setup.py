7fc000c666ead59307a596237e59ddda966fac11
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-05-18 17:53:33 -0700

    Minimal setup.py added.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='boablend',
+      version='0.1',
+      description='A Python module to enhance and simplify using Blender\'s Python API.',
+      url='https://github.com/jimmygizmo/boablend',
+      author='Jimmy Gizmo',
+      author_email='',
+      license='GPL v3',
+      packages=['boablend'],
+      zip_safe=False)
+
+
+      ##
+      #
