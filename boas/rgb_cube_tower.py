3541a65ca9351f8449a18d7bd2637c9b7c2b5601
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-05-20 01:12:36 -0700

    tower generation code mostly complete in first boa: rgb_cube_tower but needs more scene and anim setup steps. basic tower gen with RBG works good.

--- boas/rgb_cube_tower.py
+++ boas/rgb_cube_tower.py
@@ -2,7 +2,6 @@
 ####################################################################################################
 # BOA: rgb_cube_tower
 
-
 # This 'import bpy' will show as broken in the IDE if bpy is not installed in the external
 # environment, however since this script is invoked via exec() from within the Blender internal
 # Python environment, it will be imported successfully there.
@@ -42,6 +41,14 @@ importlib.reload(boablend.camera)
 ####################################################################################################
 
 
+# Camera Settings
+
+# These camera settings are great for the animation of the tower falling and shattering and were
+# used for the rendering of the following YouTube video:
+# https://www.youtube.com/watch?v=1j7_nHfTeaw
+# The FOV represents a fairly wide-angle lense which helps to capture everything from the beginning
+# of the tower's fall to the cubes that roll out on the surface towards the camera.
+
 rgb_cube_tower_camera_settings = {
     'name': 'RGB Cube Tower Camera',
     'comment': 'boablend.Camera settings for the RGB Cube Tower project',
@@ -55,26 +62,138 @@ rgb_cube_tower_camera_settings = {
     'render_resolution_y': 480,
     'scene.camera.data.angle': 88.22523942116491
 }
+# 'rot_eul0x_deg' is the symbol for 'scene.camera.rotation_euler[0] in degrees'  # X
+# 'rot_eul1y_deg' is the symbol for 'scene.camera.rotation_euler[1] in degrees'  # Y
+# 'rot_eul2z_deg' is the symbol for 'scene.camera.rotation_euler[2] in degrees'  # Z
 
+
+# Tower/Cube Settings
+
+# This version now creates a custom material for each cube and maps the entire RGB color space into the
+# XYZ space based on the position/index of each cube. Pure black to pure white is fully mapped across RGB/XYZ space.
+
+# This code will be compiled and run inside Blender using the built in Python
+# interpreter which is a version 2.x interpreter.
+
+# This code is compatible with Blender 2.79b. Cube size set via 'radius='.
+# This code is not compatible with Blender 2.80b. Cube size set via 'size='.
+# When porting between the two version note that size = 2 * radius.
+
+# The cube origin is in the volumetric center of the cube as instantiated by
+# bpy.ops.mesh.primitive_cube_add(). Obviously this is necessary to know in order
+# to perform location calculations in the main execution code.
+
+# Dimensions of the tower of cubes as number of cubes:
+cubes_x_width = 8
+cubes_y_depth = 8
+cubes_z_height = 24
+
+# Cube dimensions
+cube_radius = 1
+cube_side_length = 2 * cube_radius
+# Version 2.80b NOTE: Change the attribute name from 'radius' to 'size' and use cube_side_length
+# as the radius. Make sure to change the corresponding variable names for consistency.
+
+cube_defaults = {
+    'xloc': 0,
+    'yloc': 0,
+    'zloc': 0,
+    'radius': cube_radius,
+    'mass': 20,
+    'collision_shape': 'BOX',
+    'friction': 1,
+    'use_margin': True,
+    'collision_margin': 0,
+    'linear_damping': 0.35,
+    'angular_damping': 0.6,
+    'color_x': 0.5,
+    'color_y': 0.5,
+    'color_z': 0.5
+}
+
+
+################################## FUNCTION AND CLASS DEFINITIONS ##################################
+
+
+def instantiate_cube(cube):
+    # Create the mesh object.
+    bpy.ops.mesh.primitive_cube_add(
+        radius = cube['radius'],
+        location = (cube['xloc'], cube['yloc'], cube['zloc'])
+    )
+
+    # Add and adjust the physics.
+    bpy.ops.rigidbody.object_add()
+    obj = bpy.context.object  # For more concise code.
+    obj.rigid_body.mass = cube['mass']
+    obj.rigid_body.collision_shape = cube['collision_shape']
+    obj.rigid_body.friction = cube['friction']
+    obj.rigid_body.use_margin = cube['use_margin']
+    obj.rigid_body.collision_margin = cube['collision_margin']
+    obj.rigid_body.linear_damping = cube['linear_damping']
+    obj.rigid_body.angular_damping = cube['angular_damping']
+
+    mat_name = 'mat_' + str(cube['color_x']) + \
+                  '_' + str(cube['color_y']) + \
+                  '_' + str(cube['color_z'])
+    mat = bpy.data.materials.new(name=mat_name)
+    mat.diffuse_color = (cube['color_x'], cube['color_y'], cube['color_z'])
+
+    bpy.ops.object.mode_set(mode='OBJECT')  # Can't assign materials in editmode. Enter object mode.
+
+    bpy.context.object.active_material = mat
 
 
 ########################################## MAIN EXECUTION ##########################################
 
 
-# New instance of boablend.Camera with the specified settings:
-#main_camera = boablend.camera.Camera(bpy, cam=rgb_cube_tower_camera_settings)
+# Camera Setup
 
-# New instance of boablend.Camera which will use the class default settings:
-main_camera = boablend.camera.Camera(bpy)
+# New instance of boablend.Camera with the specified settings:
+main_camera = boablend.camera.Camera(bpy, cam=rgb_cube_tower_camera_settings)
 
 # Apply the camera settings currently stored in this instance to the current blend file:
-#main_camera.apply_camera()
+main_camera.apply_camera()
 
 # Read the camera settings in the current blend file and store them in this instance:
-main_camera.get_camera()
+#main_camera.get_camera()
 
 # Log the camera settings currently stored in this instance to the console:
 main_camera.log_camera()
+
+
+# Tower Construction
+
+current_z_position = 0
+
+for a in range(0, cubes_z_height):
+    current_z_position += cube_side_length
+    current_y_position = 0
+    #color_z = (a + 1) / cubes_z_height
+    color_z = a / cubes_z_height
+    for b in range(0, cubes_x_width):
+        current_y_position += cube_side_length
+        current_x_position = 0
+        #color_x = (b + 1) / cubes_x_width
+        color_x = b / cubes_x_width
+        for c in range(0, cubes_y_depth):
+            cube = cube_defaults
+            #color_y = (c + 1) / cubes_y_depth
+            color_y = c / cubes_y_depth
+            cube['xloc'] = current_x_position + cube_side_length
+            cube['yloc'] = current_y_position - cube_side_length
+            cube['zloc'] = current_z_position - cube_side_length
+            cube['color_x'] = color_x
+            cube['color_y'] = color_y
+            cube['color_z'] = color_z
+            instantiate_cube(cube)
+            current_x_position += cube_side_length
+
+
+# World and Animation Setup
+
+# - Go into World, Rigid Body Physics
+
 
 
 ##
