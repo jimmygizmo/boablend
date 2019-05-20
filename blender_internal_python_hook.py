3de739374fd6038c6074ad2d2422aa90936f3b0f
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-05-19 17:40:08 -0700

    implemented dispatching from the entry point. projects/sub-modules/mini-apps now called boas. first boa inside new boas dir. tests good. cleanup needed.

--- blender_internal_python_hook.py
+++ blender_internal_python_hook.py
@@ -23,6 +23,13 @@ verbose = True
 # code. It may never be necessary to change this and in fact it may not be recommended to. To fork
 # or customize execution in your Boablend project, you will probably have a better method and
 # location to do this within the entry point file itself and that would usually be more appropriate.
+#
+# TODO: Implement a dispatch mechanism in the entry point code so that some externalized and
+# and configurable switching mechanism will select which project-specific code to execute. A naming
+# convention is needed here. 'Projects' is the most immediate and simple concept, leading to:
+# 'bloablend project'. The nomenclature will be under consideration until this starts getting
+# implemented shortly. An obvious concept here is to have only generic code in the entry point as
+# well as to enble important usability features.
 
 # The following path must be expressed:
 # RELATIVE TO THE CURRENT BLEND FILE CONTAINING THIS HOOK CODE.
@@ -33,14 +40,15 @@ boablend_entry_point = 'boablend_start.py'
 # NOTE: IDE Indication of bpy import errors:
 # Since bpy is imported here within Blenders internal Python environment, we do not need to import
 # bpy in the external python code, and that's not currently possible with the setup this project
-# currently is using anyhow. This project is setup to use the VSCode (or Atom, IDEA CE, or a similar
-# IDE) to edit all of the source code. Under the current setup bpy is not yet installed externally
-# due to compilation errors in the first pass installation attempt, and so any 'import bpy'
-# statements done in external python files will show an error in the IDE.
+# currently is using anyhow. This project is setup to use the VSCode (or Atom, IDEA CE, or a
+# similar) IDE to edit all of the source code. Under the current setup bpy is not yet installed
+# externally due to compilation errors in the first pass installation attempt, and so any
+# 'import bpy' statements done in external python files will show an error in the IDE.
 #
-# Note that a small piece of hook code will reside in the blender file, but this will likely always
-# be a very small amount of code. If any changes are ever needed to the hook code or if it is
-# updated along with this project, you will need to manually (cut/paste) the updated hook code.
+# A small piece of hook code resides in the blender file, but this will likely always be a very
+# small amount of code. If any changes are ever needed to the hook code or if it is updated along
+# with this project, you will need to manually (cut/paste) the updated hook code back into the
+# blend file Text object.
 
 
 if verbose:
