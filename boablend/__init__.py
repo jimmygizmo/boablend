f13598ca5d0ff546d20ef1437f57c47d65d4b765
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-06-03 02:58:38 -0700

    making module __inits__ work for directory-level namespace imports. one down two more to go.

--- boablend/__init__.py
+++ boablend/__init__.py
@@ -1,16 +1,21 @@
 
 ####################################################################################################
-# This __init__.py file must be in place for module structure but currently has no functional role
-# in boablend. 'import boabled' would import the content of this file if there was any.
-# Currently 'import boablend' does not do anything and causes no error. This file is
-# where that code would go, since this is a directory-level namespace.
-#
-# All modules in boablend must be imported individually to use. e.g.
-# 'import boablend.util'
-# 'import boablend.constants'
-# 'import boablend.primitive.cube'
-#
-# 
+
+from . import primitive
+from . import light
+from . import camera
+# TODO: Maybe do this like user code does:
+# from . import constants as CONST
+from . import constants
+from . import util
+
+__all__ = [
+    'primitive',
+    'light',
+    'camera',
+    'constants',
+    'util'
+    ]
 
 
 ##
