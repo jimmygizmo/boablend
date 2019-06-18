5c7a3544434f05e7968d2e728e669e3e1a8368fe
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-06-18 15:20:25 -0700

    background job viper exposes blender render bug with --background. option bypassed.

--- vipers/background_job_vp.sh
+++ vipers/background_job_vp.sh
@@ -27,13 +27,27 @@ mkdir -p $TEMP_DIR
 
 ####################################################################################################
 
+# This viper is intended to use the --background Blender option but when used the rendered image
+# is just solid black. If the --background option is removed, thus allowing the GUI to open, the
+# the code still runs the same, but the image is rendered and output correctly. This could be a bug
+# in Blender or a problem in the Belder template code from which this viper is derived.
+# Unfortunately, using --background was one of the main points of this viper.
+# So, here, --background has been removed.
 /Applications/Blender/blender.app/Contents/MacOS/blender \
---background \
 --factory-startup \
 --python "$PROJECT_DIR/vipers/background_job_vp.py" -- \
---text="Background Job Test" \
+--text="Boablend" \
 --render="$TEMP_DIR/bg_job_test" \
 --save="$TEMP_DIR/bg_job_test.blend"
+
+
+# /Applications/Blender/blender.app/Contents/MacOS/blender \
+# --background \
+# --factory-startup \
+# --python "$PROJECT_DIR/vipers/background_job_vp.py" -- \
+# --text="Boablend" \
+# --render="$TEMP_DIR/bg_job_test" \
+# --save="$TEMP_DIR/bg_job_test.blend"
 
 
 ##
